808ecce539d4e46db3192ef4eefbfb01ab0e3bda
PDP-PortalDeProcessos <gabriel.araujo@velotax.com.br>
2025-07-28 09:32:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // ================== CONFIGURAÇÕES ==================
     // ⚠️ ATENÇÃO: Verifique se esta URL é a URL da sua ÚLTIMA implantação do Google Apps Script.
-    const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyVh50RfKRPBhsofmyQWF6oeZKdzMfYiszZSS8CMV68mvzwospP6EjAdVGDZFBAqQCw/exec";
+    const BACKEND_URL = "https://script.google.com/macros/s/AKfycby5VaJxwhJayqOTvpri8ycl8CK-mgGK-T_zTmdzIpx-XjDJ6wzgnTa-57Z7TMOeX1yl/exec";
     
     const DOMINIO_PERMITIDO = "@velotax.com.br";
     const CLIENT_ID = '827325386401-ahi2f9ume9i7lc28lau7j4qlviv5d22k.apps.googleusercontent.com';
